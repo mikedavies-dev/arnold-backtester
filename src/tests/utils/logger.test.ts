572e5ea5a05a64d151fdc9518f6ff7f7c86878da
miked <miked@antfx.com>
2022-01-11 20:23:20 +0100

    Coverage test

---
+++ src/tests/utils/logger.test.ts
@@ -0,0 +1,8 @@
+import Logger from '../../utils/logger';
+
+test('file exists', async () => {
+  jest.spyOn(console, 'log').mockImplementation(() => {});
+  const log = Logger('Test Logger');
+  expect(log).toMatchInlineSnapshot(`[Function]`);
+  log('Test Logger');
+});
---
+++ src/tests/utils/module.test.ts
@@ -0,0 +1,16 @@
+import {loadStrategy} from '../../utils/module';
+
+test('Load valid test module', async () => {
+  const modulePath = `../strategies/sample.ts`;
+
+  const strategy = await loadStrategy(modulePath);
+
+  expect(strategy).toMatchInlineSnapshot(`
+    Object {
+      "extraSymbols": Array [
+        "SPY",
+      ],
+      "init": [Function],
+    }
+  `);
+});
