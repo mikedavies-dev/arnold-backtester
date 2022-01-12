efadf9cffe0d70cf37fa69caf8c3bf5553d3f6c6
miked <miked@antfx.com>
2022-01-12 14:54:11 +0100

    Add module base test

---
+++ src/strategies/sample.ts
@@ -0,0 +1,3 @@
+export function init() {}
+
+export const extraSymbols = ['SPY'];
--- src/tests/utils/module.test.ts
+++ src/tests/utils/module.test.ts
@@ -1,18 +1,14 @@
-// import {loadStrategy} from '../../utils/module';
+import {loadStrategy} from '../../utils/module';
 
 test('Load valid test module', async () => {
-  /*
   const modulePath = `../strategies/sample.ts`;
 
   const strategy = await loadStrategy(modulePath);
 
-  expect(strategy).toMatchInlineSnapshot(`
-    Object {
-      "extraSymbols": Array [
-        "SPY",
-      ],
-      "init": [Function],
-    }
+  expect(strategy?.extraSymbols).toMatchInlineSnapshot(`
+    Array [
+      "SPY",
+    ]
   `);
-  */
+  expect(strategy?.init).toMatchInlineSnapshot(`[Function]`);
 });
