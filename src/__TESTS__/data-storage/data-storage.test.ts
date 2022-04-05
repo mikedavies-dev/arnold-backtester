37b7fb3dae89d3d54c71041b9de106ba64e4276a
miked <miked@antfx.com>
2022-04-05 17:54:21 +0100

    More test coverage

--- src/__TESTS__/data-storage/data-storage.test.ts
+++ src/__TESTS__/data-storage/data-storage.test.ts
@@ -1,10 +1,73 @@
 import {ensureDataIsAvailable} from '../../utils/data-storage';
+import {getTestDate} from '../test-utils/tick';
+
+import {connect, disconnect, resetDatabase} from '../../utils/db';
+
+jest.mock('../../utils/data-provider');
 
 // Mocks
-import {hasTsForSymbolAndDate} from '../../utils/tick-storage';
 import {createDataProvider} from '../../utils/data-provider';
-import {listAvailablePeriodsForSymbolAndDate} from '../../utils/db';
 
-test('check data storage', () => {
-  expect(1).toBe(1);
+const createDataProviderMock = createDataProvider as jest.MockedFunction<
+  typeof createDataProvider
+>;
+
+describe('mongo db tests', () => {
+  beforeAll(async () => {
+    // Reset the test database
+    await resetDatabase();
+
+    // Connect again
+    await connect();
+  });
+
+  afterAll(async () => {
+    await disconnect();
+
+    createDataProviderMock.mockReset();
+  });
+
+  test('check data storage', async () => {
+    const mockProvider = {
+      init: jest.fn(async () => {}),
+      getTimeSeries: jest.fn(async () => {
+        return [
+          {
+            open: 1,
+            high: 1,
+            low: 1,
+            close: 1,
+            volume: 1,
+            time: 'TODO',
+          },
+        ];
+      }),
+    };
+    createDataProviderMock.mockReturnValue(mockProvider);
+
+    await ensureDataIsAvailable({
+      symbols: ['ZZZZ'],
+      dates: [getTestDate()],
+      log: () => {},
+    });
+
+    // Check variables
+    expect(mockProvider.getTimeSeries).toBeCalledWith(
+      'ZZZZ',
+      getTestDate(),
+      'm1',
+    );
+
+    expect(mockProvider.getTimeSeries).toBeCalledWith(
+      'ZZZZ',
+      getTestDate(),
+      'm60',
+    );
+
+    expect(mockProvider.getTimeSeries).toBeCalledWith(
+      'ZZZZ',
+      getTestDate(),
+      'daily',
+    );
+  });
 });
