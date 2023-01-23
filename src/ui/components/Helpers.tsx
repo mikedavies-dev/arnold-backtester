2150978868a73eab8a9cfbf89276616dd52d1fff
Mike Davies <miked@antfx.com>
2023-01-23 17:48:58 +0100

    remove web ui

--- src/ui/components/Helpers.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const AlignRight = styled.div`
-  text-align: right; ;
-`;
--- src/web/trader/index.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDom from 'react-dom';
-
-const container = document.getElementById('container');
-
-if (container) {
-  ReactDom.render(<div>Live Trade Results</div>, container);
-}
